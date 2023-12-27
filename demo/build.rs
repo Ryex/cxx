b93acccdc9093a0195395611d5264a8927347596
Dima Dorezyuk <ddo@qwello.eu>
2023-12-27 16:47:45 +0100

    try fix windows

--- demo/build.rs
+++ demo/build.rs
@@ -2,6 +2,7 @@ fn main() {
     cxx_build::bridge("src/main.rs")
         .file("src/blobstore.cc")
         .std("c++17")
+        .flag_if_supported("/Zc:__cplusplus")
         .compile("cxxbridge-demo");
 
     println!("cargo:rerun-if-changed=src/main.rs");
